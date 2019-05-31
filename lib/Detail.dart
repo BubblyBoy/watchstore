e98f9be53bee3a1cbf00b468869f53b50142d7b0
BubblyBoy <aliyunus291@gmail.com>
2019-06-01 02:47:25 +0300

    details

---
+++ lib/Detail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class productde extends StatefulWidget {
+  productde({Key key}) : super(key: key);
+
+  _productdeState createState() => _productdeState();
+}
+
+class _productdeState extends State<productde> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+
+  }
+}
